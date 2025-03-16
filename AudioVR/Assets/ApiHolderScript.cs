ae50efd64bd0bd3be36a169ca79ea3fdae82e77f
IbrmSerhat <97800760+IbrmSerhat@users.noreply.github.com>
2025-03-17 01:58:17 +0300

    Holders

---
+++ AudioVR/Assets/ApiHolderScript.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApiHolderScript : MonoBehaviour
+{
+    public string apiMassage1;
+    public string apiMassage2;
+    public string apiMassage3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AudioVR/Assets/ScriptHolder.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptHolder : MonoBehaviour
+{
+    public int SceneIndex;
+
+    public string Stroy1 = "A 30-year-old female patient has been experiencing imbalance and recurrent vertigo attacks for the past year. The vertigo is more pronounced on the left side and is triggered by loud sounds, including her own voice, as well as coughing, sneezing, and straining. She reports increased difficulty in speech comprehension in noisy environments but denies tinnitus. The patient has no history of trauma or any surgical procedures. Her other medical history is unremarkable.";
+    public string Stroy2 = "A 45-year-old female patient presents with hearing loss, tinnitus resembling a ringing sound, and difficulty distinguishing speech in noisy environments. She does not report vertigo, dizziness, or any additional ear complaints. She is being monitored for hypertension as a comorbid condition.";
+    public string Stroy3 = ": A 25-year-old female patient requires a hearing examination before starting her newly accepted job. She does not report any complaints of ear pressure, tinnitus, pain, or a feeling of fullness. She has no known comorbidities.";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
